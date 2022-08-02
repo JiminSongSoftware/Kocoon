c306e2d0be16fd6dfcaea376ad99db859ffb87be
Daniel Lee <daniellee9707@gmail.com>
2022-08-02 14:32:03 -0700

    Added expo splash screen

--- Kocoon/App.js
+++ Kocoon/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomNavigator from './src/components/BottomNavigator';
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  // const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        // setAppIsReady(true);
+        SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }, []);
+
   return (
     <NavigationContainer>
-      <BottomNavigator />
+       <BottomNavigator />
     </NavigationContainer>
   );
 }
