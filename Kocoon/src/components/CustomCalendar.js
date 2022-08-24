bac604574288ee8746529ee10456974f6968d550
Daniel Lee <daniellee9707@gmail.com>
2022-08-24 15:37:16 -0700

    Modified calendar styling

--- Kocoon/src/components/CustomCalendar.js
+++ Kocoon/src/components/CustomCalendar.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { View, StyleSheet } from 'react-native';
-import { Calendar } from "react-native-calendars";
+import { View, StyleSheet, Text } from 'react-native';
+import { Calendar, } from "react-native-calendars";
+
 
 function CustomCalendar() {
     return (
-        <View>
             <Calendar
+                style={{borderRadius:5}}
+                theme={{
+                    calendarBackground: '#DED6D3',
+                    dayTextColor: '#000000',
+                }}
                 // Initially visible month. Default = now
-                initialDate={'2012-03-01'}
+                // initialDate={'2022-01-01'}
                 // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                minDate={'2012-05-10'}
+                // minDate={'2022-01-01'}
                 // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                maxDate={'2012-05-30'}
+                // maxDate={'2023-12-31'}
                 // Handler which gets executed on day press. Default = undefined
                 onDayPress={day => {
                     console.log('selected day', day);
@@ -21,7 +26,7 @@ function CustomCalendar() {
                     console.log('selected day', day);
                 }}
                 // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-                monthFormat={'yyyy MM'}
+                monthFormat={'MMMM'}
                 // Handler which gets executed when visible month changes in calendar. Default = undefined
                 onMonthChange={month => {
                     console.log('month changed', month);
@@ -29,18 +34,18 @@ function CustomCalendar() {
                 // Hide month navigation arrows. Default = false
                 hideArrows={true}
                 // Replace default arrows with custom ones (direction can be 'left' or 'right')
-                renderArrow={direction => <Arrow />}
+                // renderArrow={direction => <Arrow />}
                 // Do not show days of other months in month page. Default = false
                 hideExtraDays={true}
                 // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
                 // day from another month that is visible in calendar page. Default = false
-                disableMonthChange={true}
+                disableMonthChange={false}
                 // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
                 firstDay={1}
                 // Hide day names. Default = false
-                hideDayNames={true}
+                hideDayNames={false}
                 // Show week numbers to the left. Default = false
-                showWeekNumbers={true}
+                showWeekNumbers={false}
                 // Handler which gets executed when press arrow icon left. It receive a callback can go back month
                 onPressArrowLeft={subtractMonth => subtractMonth()}
                 // Handler which gets executed when press arrow icon right. It receive a callback can go next month
@@ -52,18 +57,16 @@ function CustomCalendar() {
                 // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
                 disableAllTouchEventsForDisabledDays={true}
                 // Replace default month and year title with custom one. the function receive a date as parameter
-                renderHeader={date => {
-                    /*Return JSX*/
-                }}
+                // renderHeader={date => {
+                    
+                // }}
                 // Enable the option to swipe between months. Default = false
                 enableSwipeMonths={true}
             />
-        </View>
     );
 }
 
 const styles = StyleSheet.create({
-
 });
 
 export default CustomCalendar;
